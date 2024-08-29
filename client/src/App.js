56a1b86a5279d8492172fdb5f80e39537c1fc522
rajsha10 <rajsha8617@gmail.com>
2024-08-29 22:43:11 +0530

    added ethers new

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import Upload from './contracts/Upload.sol/Upload.json';
 import { useState, useEffect } from "react";
-import { ethers } from "ethers";
+import { ethers } from 'ethers';
 import FileUpload from "./components/FileUpload";
 import Display from "./components/Display";
 import Modal from "./components/Modal";
@@ -13,7 +13,7 @@ function App() {
   const [modalOpen, setModalOpen] = useState(false);
 
   useEffect(() => {
-    const provider = new ethers.providers.Web3Provider(window.ethereum);
+    const provider = new ethers.providers.JsonRpcProvider();
 
     const loadProvider = async () => {
       if (provider) {
